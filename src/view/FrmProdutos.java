d509751b38a29f2c2b7309a38db9998288ab3fde
Adauto Bueno <adauto.bueno@gmail.com>
2022-12-27 23:49:31 -0300

    Form produtos

--- src/view/FrmProdutos.java
+++ src/view/FrmProdutos.java
@@ -6,6 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTable;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.FlowLayout;
 
 public class FrmProdutos extends JFrame {
 
@@ -36,11 +45,15 @@ public class FrmProdutos extends JFrame {
 	 */
 	public FrmProdutos() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 520, 347);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblCadas = new JLabel("Cadastro de Produtos");
+		lblCadas.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblCadas.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblCadas);
 	}
-
 }

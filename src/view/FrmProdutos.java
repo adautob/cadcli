08a412b3fac83dc3e89f0670f38fff3cfa1f0723
Adauto Bueno <adauto.bueno@gmail.com>
2022-12-28 23:42:54 -0300

    FrmProdutos

--- src/view/FrmProdutos.java
+++ src/view/FrmProdutos.java
@@ -15,6 +15,8 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormSpecs;
 import com.jgoodies.forms.layout.RowSpec;
 import java.awt.FlowLayout;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
 
 public class FrmProdutos extends JFrame {
 
@@ -23,6 +25,7 @@ public class FrmProdutos extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -44,16 +47,42 @@ public class FrmProdutos extends JFrame {
 	 * Create the frame.
 	 */
 	public FrmProdutos() {
+		final Integer LARGURA = 520;
+		final Integer ALTURA = 345;
+		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 520, 347);
+		setBounds(100, 100, LARGURA, ALTURA);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.setLayout(null);
 		
 		JLabel lblCadas = new JLabel("Cadastro de Produtos");
+		lblCadas.setBounds(149, 10, 196, 19);
 		lblCadas.setFont(new Font("Dialog", Font.BOLD, 16));
 		lblCadas.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblCadas);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(2, ALTURA-203, LARGURA-11, ALTURA/2);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"Descri\u00E7\u00E3o", "Pre\u00E7o"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(200);
+		table.getColumnModel().getColumn(0).setMinWidth(200);
+		table.getColumnModel().getColumn(1).setPreferredWidth(50);
+		table.getColumnModel().getColumn(1).setMinWidth(50);
+		
+	
 	}
 }

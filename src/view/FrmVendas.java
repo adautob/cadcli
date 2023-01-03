d4585d67803ade4cad7314dcf864d419698d973a
Adauto Bueno <adauto.bueno@gmail.com>
2023-01-02 23:41:05 -0300

    FrmVendas

--- src/view/FrmVendas.java
+++ src/view/FrmVendas.java
@@ -1,11 +1,21 @@
 package view;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import java.awt.Rectangle;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
 
 public class FrmVendas extends JFrame {
 
@@ -14,6 +24,8 @@ public class FrmVendas extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JScrollPane scrollPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -34,14 +46,32 @@ public class FrmVendas extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	@SuppressWarnings("serial")
 	public FrmVendas() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 500, 400);
+		setBounds(100, 100, 591, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBackground(UIManager.getColor("Button.focus"));
+		scrollPane.setBounds(2, 180, this.getWidth()-20, 140);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column", "New column"
+			}
+		));
+		table.setBorder(new LineBorder(new Color(0, 0, 0)));
+		scrollPane.setViewportView(table);
 	}
-
 }

c010fee8f2829a0212b6fcdd4d3020dcf2880d62
Adauto Bueno <adauto.bueno@gmail.com>
2023-01-26 21:21:23 -0300

    PesqProd e PesqCli

--- src/view/PesqCli.java
+++ src/view/PesqCli.java
@@ -1,12 +1,14 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class PesqCli extends JDialog {
 
@@ -14,7 +16,8 @@ public class PesqCli extends JDialog {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private JTextField textField;
 
 
 
@@ -23,16 +26,15 @@ public class PesqCli extends JDialog {
 	 */
 	public PesqCli(FrmVendas mf, String title, boolean modal) {
 		super(mf, title, modal);
+		setResizable(false);
 		setBounds(100, 100, 300, 200);
 		setLocationRelativeTo(mf);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		getContentPane().setLayout(null);
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBounds(0, 131, 290, 35);
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			getContentPane().add(buttonPane);
 			{
 				JButton okButton = new JButton("OK");
 				okButton.setActionCommand("OK");
@@ -45,6 +47,21 @@ public class PesqCli extends JDialog {
 				buttonPane.add(cancelButton);
 			}
 		}
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(3, 56, 285, 75);
+		getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JLabel lblPesquisarClientePor = new JLabel("Digite o nome");
+		lblPesquisarClientePor.setBounds(3, 12, 100, 15);
+		getContentPane().add(lblPesquisarClientePor);
+		
+		textField = new JTextField();
+		textField.setBounds(3, 30, 285, 19);
+		getContentPane().add(textField);
+		textField.setColumns(10);
 	}
-
 }
--- src/view/PesqProd.java
+++ src/view/PesqProd.java
@@ -1,12 +1,14 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class PesqProd extends JDialog {
 
@@ -14,7 +16,8 @@ public class PesqProd extends JDialog {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private JTextField textField;
 
 
 
@@ -23,16 +26,15 @@ public class PesqProd extends JDialog {
 	 */
 	public PesqProd(FrmVendas mf, String title, boolean modal) {
 		super(mf, title, modal);
+		setResizable(false);
 		setBounds(100, 100, 300, 200);
 		setLocationRelativeTo(mf);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		getContentPane().setLayout(null);
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBounds(0, 131, 290, 35);
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			getContentPane().add(buttonPane);
 			{
 				JButton okButton = new JButton("OK");
 				okButton.setActionCommand("OK");
@@ -45,6 +47,21 @@ public class PesqProd extends JDialog {
 				buttonPane.add(cancelButton);
 			}
 		}
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(3, 56, 285, 75);
+		getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JLabel lblPesquisarClientePor = new JLabel("Digite o produto");
+		lblPesquisarClientePor.setBounds(3, 12, 120, 15);
+		getContentPane().add(lblPesquisarClientePor);
+		
+		textField = new JTextField();
+		textField.setBounds(3, 30, 285, 19);
+		getContentPane().add(textField);
+		textField.setColumns(10);
 	}
-
 }
